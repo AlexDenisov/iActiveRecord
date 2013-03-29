dd2affe132928cd3db3fcff18ccdaead838e6952
Ryan Laughlin <rofreg@gmail.com>
2013-03-29 14:33:55 -0400

    Fix NSDecimalNumber sql representation

--- iActiveRecord/Classes/NSDecimalNumber+sqlRepresentation.m
+++ iActiveRecord/Classes/NSDecimalNumber+sqlRepresentation.m
@@ -10,8 +10,17 @@
 
 @implementation NSDecimalNumber (sqlRepresentation)
 
++ (NSLocale *)posixLocale {
+    static NSLocale *posixLocale;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        posixLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+    });
+    return posixLocale;
+}
+
 - (NSString *)toSql {
-    return [NSString stringWithFormat:@"%f", [self floatValue]];
+    return [self descriptionWithLocale:[NSDecimalNumber posixLocale]];
 }
 
 + (NSString *)sqlType {
@@ -19,7 +28,7 @@
 }
 
 + (id)fromSql:(NSString *)sqlData {
-    return [NSDecimalNumber decimalNumberWithString:sqlData];
+    return [NSDecimalNumber decimalNumberWithString:sqlData locale:[NSDecimalNumber posixLocale]];
 }
 
 @end
